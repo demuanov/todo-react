012742b3aaf0750cd65ec440ce64bac9729b3101
demunaov <demuanov@gmail.com>
2024-07-13 20:40:27 +0300

    Split to different components

---
+++ src/components/taskArea/taskArea.tsx
@@ -0,0 +1,11 @@
+import { Grid } from '@mui/material';
+import React, { FC, ReactElement } from 'react';
+
+
+export const TaskArea:FC = (): ReactElement => {
+  return(
+    <Grid item md={8} px={4} >
+      <h2>Content area</h2>
+    </Grid>
+  )
+}
