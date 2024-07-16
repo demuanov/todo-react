beee8bd6ee7afd28ab915752cedbf8a80aa65c85
demunaov <demuanov@gmail.com>
2024-07-16 22:48:10 +0300

    Add Task component

--- src/components/taskArea/taskArea.tsx
+++ src/components/taskArea/taskArea.tsx
@@ -1,10 +1,50 @@
-import { Grid } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import React, { FC, ReactElement } from 'react';
+import { format } from 'date-fns';
+import { TaskCounter } from '../taskCounter/taskCounter';
+import { Status } from '../createTaskForm/enums/Status';
+import { Task } from '../task/task';
 
 export const TaskArea: FC = (): ReactElement => {
   return (
     <Grid item md={8} px={4}>
-      <h2>Content area</h2>
+      <Box mb={8} px={4}>
+        <h2>
+          Status Of Your Tasks As On
+          {format(new Date(), 'PPP')}
+        </h2>
+      </Box>
+
+      <Grid
+        container
+        display="flex"
+        justifyContent="center"
+      >
+        <Grid
+          item
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-around"
+          alignItems="center"
+          md={10}
+          xs={12}
+          mb={8}
+        >
+          <TaskCounter status={Status.todo} count={0} />
+          <TaskCounter status={Status.inProgress} />
+          <TaskCounter status={Status.completed} />
+        </Grid>
+        <Grid
+          item
+          display="flex"
+          flexDirection="column"
+          xs={10}
+          mb={8}
+        >
+          <Task></Task>
+          <Task></Task>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
