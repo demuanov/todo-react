510f3f10b6ff64b2ceef7c76c3f61658a4867377
demunaov <demuanov@gmail.com>
2024-08-28 22:24:57 +0300

    Add state manager for component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import React, { FC, ReactElement } from 'react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { customTheme } from './theme/customTheme';
+import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import { Dashboard } from './pages/dashboard/dashboard';
 
+const queryClient = new QueryClient()
+
 const App: FC = (): ReactElement => {
   return (
+    <QueryClientProvider client={queryClient}>
     <ThemeProvider theme={customTheme}>
       <CssBaseline />
       <Dashboard></Dashboard>
     </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false}/>
+    </QueryClientProvider>      
   );
 };
 export default App;
